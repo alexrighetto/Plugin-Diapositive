05dd50f6ea2b3bb3e2e676a3bc499d3dd9af689b
Alex <alexrighetto@gmail.com>
2013-12-14 23:31:57 +0100

    modifiche

--- diapositive.php
+++ diapositive.php
@@ -47,16 +47,16 @@ if ( is_admin() ) {
     require_once( dirname(__FILE__).'/updater.php' );
 	$config = array(
 			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-			'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-			'api_url' => 'https://api.github.com/repos/username/repository-name', // the github API url of your github repo
-			'raw_url' => 'https://raw.github.com/username/repository-name/master', // the github raw url of your github repo
-			'github_url' => 'https://github.com/username/repository-name', // the github url of your github repo
-			'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the github repo
+			'proper_folder_name' => 'Plugin-Diapositive', // this is the name of the folder your plugin lives in
+			'api_url' => 'https://api.github.com/repos/alexrighetto/Plugin-Diapositive', // the github API url of your github repo
+			'raw_url' => 'https://raw.github.com/alexrighetto/Plugin-Diapositive', // the github raw url of your github repo
+			'github_url' => 'https://github.com/alexrighetto/Plugin-Diapositive', // the github url of your github repo
+			'zip_url' => 'https://github.com/alexrighetto/Plugin-Diapositive/zipball/master', // the zip url of the github repo
 			'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 			'requires' => '3.0', // which version of WordPress does your plugin require?
 			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
 			'readme' => 'README.md', // which file to use as the readme for the version number
-			'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
+			'access_token' => '40e2d0d6b62404e70ed89537a18983483eed3e0a', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
 		);
 		new WP_GitHub_Updater($config);
 }
